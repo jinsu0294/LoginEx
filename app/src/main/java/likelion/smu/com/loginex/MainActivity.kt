4082746db1d0d8e39a494c17680a3d2b0c991563
jinsu0294 <jinsususu@gmail.com>
2019-08-25 16:15:25 +0900

    Google and EmailPassword LoginEX

---
+++ app/src/main/java/likelion/smu/com/loginex/MainActivity.kt
@@ -0,0 +1,176 @@
+package likelion.smu.com.loginex
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var currentUser: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_Glogin.setOnClickListener {
+            signInWithGoogle()
+
+        }
+
+        //////
+
+        btn_create.setOnClickListener {
+            var memail = et_email.text.toString()
+            var mpassword = et_password.toString()
+            createAccount(memail,mpassword)
+        }
+
+        btn_login.setOnClickListener {
+            var memail = et_email.text.toString()
+            var mpassword = et_password.toString()
+            signInWithEmail(memail,mpassword)
+
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        currentUser = auth.currentUser.toString()
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    tv_id.text =user?.email
+
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+
+            }
+    }
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    private fun signOut() {
+        auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+
+        }
+    }
+    companion object {
+        private const val TAG = "TAG"
+        private const val RC_SIGN_IN = 9001
+    }
+
+    //////////////////
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signInWithEmail(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    tv_id.text =user?.email
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        // [END sign_in_with_email]
+    }
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = et_email.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            et_email.error = "Required."
+            valid = false
+        } else {
+            et_email.error = null
+        }
+
+        val password = et_password.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            et_password.error = "Required."
+            valid = false
+        } else {
+            et_password.error = null
+        }
+
+        return valid
+    }
+}
